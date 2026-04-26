ee8aec27e547ebb0e4045e30419a910643f83454
Faiq <faiqfarooq05@gmail.com>
2026-04-26 22:15:51 +0530

    Implement user update and delete functionality; add active status to user model

--- models/userModel.js
+++ models/userModel.js
@@ -53,6 +53,11 @@ const userSchema = new mongoose.Schema({
   },
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: String,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre('save', async function () {
@@ -68,6 +73,11 @@ userSchema.pre('save', function () {
   if (!this.isModified('password') || this.isNew) return;
 
   this.passwordChangedAt = Date.now() - 1000;
+});
+
+userSchema.pre(/^find/, function () {
+  // this points to the current query
+  this.find({ active: { $ne: false } });
 });
 
 userSchema.methods.correctPassword = async function (
