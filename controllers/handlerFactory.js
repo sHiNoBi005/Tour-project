b3a019ada24c64dd1aa60accf5ada7b87f5c5897
Faiq <faiqfarooq05@gmail.com>
2026-05-09 20:05:26 +0530

    feat: add average rating calculation for reviews; implement post-save and post-findOneAnd hooks to update tour ratings

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -75,7 +75,7 @@ const getAll = (Model) =>
       .sort()
       .limitFields()
       .paginate();
-    const doc = await features.query.explain();
+    const doc = await features.query;
 
     //Send Response
     res.status(200).json({
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import Tour from './tourModel.js';
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -32,11 +33,52 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nrating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nrating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  //this points to current review
+  this.constructor.calcAverageRatings(this.tour);
+});
+
 reviewSchema.pre(/^find/, function () {
   this.populate({
     path: 'user',
     select: 'name photo',
   });
+});
+
+reviewSchema.pre(/^findOneAnd/, async function () {
+  this.r = await this.clone().findOne();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // this.findOne() won't work here, query already executed
+  if (!this.r) return;
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
